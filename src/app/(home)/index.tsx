2428c331ddcaa71e793147b6e370aca54fbe4337
Ayush Saini <ayushsaini1910@gmail.com>
2026-04-03 15:21:39 +0530

    add signin page

--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -14,9 +14,7 @@ export default function Page() {
         <Link href="/(auth)/sign-in">
           <Text>Sign in</Text>
         </Link>
-        <Link href="/(auth)/sign-up">
           <Text>Sign up</Text>
-        </Link>
       </Show>
       <Show when="signed-in">
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
